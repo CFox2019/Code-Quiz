290eba0e2f62fe0ef57d00d41377457f36e237a9
Courtney Fox <courtney.fox@me.com>
2021-01-10 12:11:36 -0500

    added start, quiz, and submit buttons in js and layout of quiz questions

---
+++ Assets/script.js
@@ -0,0 +1,97 @@
+var startButton = document.getElementById('start');
+var quizQuestions = document.getElementById('quiz');
+var submitButton = document.getElementById('submit');
+
+// Quiz Questions
+var quizQuestions = [
+    {
+        Question: "My question will go here?",
+        answers: {
+            a: "incorrect answer",
+            b: "correct answer",
+            c: "incorrect answer"
+        },
+        correctAnswer: "b"
+    },
+    {
+        Question: "My question will go here?",
+        answers: {
+            a: "incorrect answer",
+            b: "incorrect answer",
+            c: "correct answer"
+        },
+        correctAnswer: "c"
+    },
+    {
+        Question: "My question will go here?",
+        answers: {
+            a: "incorrect answer",
+            b: "incorrect answer",
+            c: "correct answer"
+        },
+        correctAnswer: "c"
+    },
+    {
+        Question: "My question will go here?",
+        answers: {
+            a: "incorrect answer",
+            b: "incorrect answer",
+            c: "correct answer"
+        },
+        correctAnswer: "c"
+    },
+    {
+        Question: "My question will go here?",
+        answers: {
+            a: "incorrect answer",
+            b: "incorrect answer",
+            c: "correct answer"
+        },
+        correctAnswer: "c"
+    },
+    {
+        Question: "My question will go here?",
+        answers: {
+            a: "incorrect answer",
+            b: "incorrect answer",
+            c: "correct answer"
+        },
+        correctAnswer: "c"
+    },
+    {
+        Question: "My question will go here?",
+        answers: {
+            a: "incorrect answer",
+            b: "incorrect answer",
+            c: "correct answer"
+        },
+        correctAnswer: "c"
+    },
+    {
+        Question: "My question will go here?",
+        answers: {
+            a: "incorrect answer",
+            b: "incorrect answer",
+            c: "correct answer"
+        },
+        correctAnswer: "c"
+    },
+    {
+        Question: "My question will go here?",
+        answers: {
+            a: "incorrect answer",
+            b: "incorrect answer",
+            c: "correct answer"
+        },
+        correctAnswer: "c"
+    },
+    {
+        Question: "My question will go here?",
+        answers: {
+            a: "incorrect answer",
+            b: "incorrect answer",
+            c: "correct answer"
+        },
+        correctAnswer: "c"
+    }
+];
